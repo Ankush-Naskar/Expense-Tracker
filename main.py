561cb7d4f64600a8f67c2ef04cb0f985b752e41a
Ankush Naskar <ankush29607@gmail.com>
2025-11-23 00:06:47 +0530

    added modules, user, gitignore

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
+from user import addChategories
+from modules import menu, Input
+
+# expenses list
 expensesList = []
 
-while True:
-    print("=====MENU=====")
-    print("1. Add Expense")
-    print("2. View Total Expense")
-    print("3. view Total Amount")
-    print("4. exit")
+# chategory list
+chategoriez = []
+chategories = ["Grocery", "Food", "Gadget", "Others"]
+chategories.extend(addChategories)
+i = 1
+for element in chategories:
+    chategoriez.append(f"{i}. {element}")
+    i += 1
 
-    choice = int(input("Enter your Choice : "))
 
-# Add Expense
+try:
+    while True:
 
-    if choice==1:
-        date= input("date of expense")
-        chategory = input("Enter chategory : ")
-        description = input("product name : ")
-        amount = float(input("Enter the amount : "))
+        menu()
+        choice = int(input("Enter your Choice : "))
 
-        expense = {
-            "date": date,
-            "category": chategory,
-            "description": description,
-            "amount": amount
-        }
+    # Add Expense
+        if choice==1:
 
-        expensesList.append(expense)
-        print("Expenses added succesfully")
+            expensesList.append(Input(chategoriez))
+            print("Expenses added succesfully")
 
-# View Total Expense
-    elif(choice == 2):
-        if len(expensesList)==0:
-            print("no expenses added")
-        else:
-            print("=== your expenses ===")
-            count= 1
+        
+
+    # View Total Expense
+        elif(choice == 2):
+            if len(expensesList)==0:
+                print("no expenses added")
+            else:
+                print("=== your expenses ===")
+                count= 1
+                for eachExpense in expensesList:
+                    print(f"{count}. {eachExpense['date']}, {eachExpense['category']}, {eachExpense['description']}, {eachExpense['amount']}")
+
+    #  view Total Amount
+        elif(choice == 3):
+            total = 0
             for eachExpense in expensesList:
-                print(f"{count}. {eachExpense['date']}, {eachExpense['category']}, {eachExpense['description']}, {eachExpense['amount']}")
+                total += eachExpense["amount"]
+            print(total)
 
-#  view Total Amount
-    elif(choice == 3):
-        total = 0
-        for eachExpense in expensesList:
-            total += eachExpense["amount"]
-        print(total)
+    # Exit
+        elif(choice == 4):
+            break
 
-# Exit
-    elif(choice == 4):
-        break
+        else:
+            print("invalid choice")
 
-    else:
-        print("invalid choice")
+except Exception as e:
+    print("error ocurs\n", e)
---
+++ user.py
@@ -0,0 +1,1 @@
+addChategories = ["cloathes"]
